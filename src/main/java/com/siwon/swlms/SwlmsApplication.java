31aa6ebc579d9c965788cfc444a358dd11f176bb
siwon <siwon8783@gmail.com>
2021-09-10 20:37:50 +0900

    swlms 초기화 설정

---
+++ src/main/java/com/siwon/swlms/SwlmsApplication.java
@@ -0,0 +1,13 @@
+package com.siwon.swlms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwlmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwlmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/siwon/swlms/SwlmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siwon.swlms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwlmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
