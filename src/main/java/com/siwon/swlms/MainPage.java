d94e0b8077d4e739fcafabe6f73f9523ff2f75db
SiwonLee <siwon@finda.co.kr>
2021-09-10 21:04:35 +0900

    request, response 및 주석 추가

--- src/main/java/com/siwon/swlms/MainPage.java
+++ src/main/java/com/siwon/swlms/MainPage.java
@@ -8,10 +8,15 @@ package com.siwon.swlms;
 // 어디서 매핑? 누가 매핑?
 // 후보군? 1. 클래스 2. 속성 3. 메소드
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@Controller
 public class MainPage {
 
     @RequestMapping("/")
@@ -19,9 +24,34 @@ public class MainPage {
         return "Index Page";
     }
 
-    @RequestMapping
-    public String hello() {
-        String msg = "<p>hello</p> <p>swlms website!!!</p>";
-        return "hello";
+    // 스프링 -> MVC (View -> 템플릿엔진 화면에 내용을 출력(html))
+    // .NET -> MVC (View -> 출력)
+    // python django -> MTV(Template -> 화면출력)
+    // 백엔드 과정 -> V(화면에 보여진 부분) -> 프론트엔드 과정
+    // V -> HTML -> 웹페이지가
+    // V -> json -> API
+
+
+    // request -> WEB -> SERVER
+    // response -> SERVER -> WEB
+    @RequestMapping("/hello")
+    public void hello(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter printWriter = response.getWriter();
+
+        String msg = "<html>" +
+                "<head>" +
+                "<meta charset=\"UTF-8\">" +
+                "</head>" +
+                "<body>" +
+                "<p>hello</p>" +
+                "<p>swlms website!!!</p>" +
+                "<p> 안녕하세요 ====> </p>" +
+                "</body>" +
+                "</html>";
+
+        printWriter.write(msg);
+        printWriter.close();
     }
 }
